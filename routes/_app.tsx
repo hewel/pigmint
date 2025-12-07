d6532f13e6ec026a51c6678c68b0a4013a58929f
hewel <hewel@vivaldi.net>
2025-12-07 18:32:42 +0800

    fix: add lang attribute to html tag for improved accessibility

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -3,7 +3,7 @@ import ThemeInitializer from "../islands/ThemeInitializer.tsx";
 
 export default define.page(function App({ Component }) {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
