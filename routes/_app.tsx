09f627ebfbea48be09160048ee517a70d3179f6a
hewel <hewel@vivaldi.net>
2026-06-28 15:57:31 +0800

    fix(app): remove stale stylesheet link

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -38,7 +38,6 @@ export default define.page(function App({ Component }) {
           href="https://fonts.googleapis.com/css2?family=Fredoka&family=Nunito:wght@400;600;700;800&display=swap"
           rel="stylesheet"
         />
-        <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
         <ThemeInitializer />
